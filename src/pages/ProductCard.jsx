96726b3734bed7fb27e188adc1fe3358795b2884
UrvashiSohaliya2511 <urvashisohaliya00@gmail.com>
2022-08-24 15:43:02 +0530

    axios

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -1,5 +1,6 @@
 import { Stars } from "../componants/Homepage.Sliders.jsx";
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductCard = () => {
   return <div>ProductCard</div>;
